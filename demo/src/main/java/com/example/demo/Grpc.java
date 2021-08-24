2e1ca85fbe186f34676f90d8e01e2a7272975a88
zhangchengji <380702562@qq.com>
2021-08-24 13:41:59 +0800

    去除测试

--- demo/src/main/java/com/example/demo/Grpc.java
+++
@@ -1,9 +0,0 @@
-//package com.example.demo;
-//import com.github.zhangchengji.proto.client.FlyingConfig;
-//
-//public interface Grpc {
-//
-//    FlyingConfig config(String namespace);
-//    boolean  listener();
-//    void shutdown();
-//}
